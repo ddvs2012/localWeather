e66c855f93b57bf350ba97d202666f742eaa5e24
Daniel Alexander <davisd49@msu.edu>
2016-12-14 01:11:35 -0500

    icons working, do backgrounds next

--- scripts/script.js
+++ scripts/script.js
@@ -11,23 +11,47 @@ $(document).ready(function(){
 			
 			$.getJSON(api, function(data){
 
+//icon
+				var icon = data.weather[0].id
+				$('#icon').html("<i class='owf owf-" + icon + " owf-5x'></i>");	
+//City Name
+				$('#location').html("<span class='type'>City: </span>"+JSON.stringify(data.name));			
+//Weather Description and backgrounds	NEEDS WORK		
+				weatherDescription = data.weather[0].main;
+				$('#weatherDescription').html("<span class='type'>Condition: </span>"+weatherDescription);
+				
+				if(weatherDescription === "Mist"){
+					$('body').css('background-image', 'url(https://static.pexels.com/photos/132983/pexels-photo-132983.jpeg)');
+				}	
+				
+				backgroundImg = {
+					thunderStormD : "200-299",
+					drizzleD : "300-399",
+					rainD : "500-599",
+					snowD : "600-699",
+					atmosphereD : "700-799",
+					clearD : "800",
+					cloudsD : "801-804",
+					extremeD : "900-999",
+				}
+				//start here
+				switch(){
+					case 0:
+						data.weather[0].main < 299
+						break;
+				}
+					
+				
+				
+
+//Sunrise and set time NEEDS WORK
+				new Date(data.sys.sunrise * 1000);
+				$('#time').html(Date);
 //Temperature				
 				kTemp = data.main.temp;
 				fTemp = Math.round(kTemp * (9/5) - 459.67);
 				cTemp = ((fTemp - 32) * (5/9));
-					$('#temp-container').html(Math.round(fTemp) + '&deg;F');
-			
-//Weather Description and backgrounds	NEEDS WORK		
-				weatherDescription = data.weather[0].description;
-			if(weatherDescription === "mist")
-				$('body').css('background-image', 'url(https://static.pexels.com/photos/132983/pexels-photo-132983.jpeg)');
-				$('#weatherDescription').html(weatherDescription);
-//City Name
-				$('#location').html(JSON.stringify(data.name));
-//Sunrise and set time NEEDS WORK
-				new Date(data.sys.sunrise * 1000);
-				$('#time').html(Date);
-				
+					$('#temp-container').html(Math.round(fTemp) + '&deg;F');				
 				
 			});
 			
