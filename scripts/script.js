dfd9e8c35fc2e839a7fe77f9dfaaf482f1853d45
Daniel Alexander <davisd49@msu.edu>
2016-12-14 16:24:36 -0500

    finished background functions, input location in future

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,11 @@
 $(document).ready(function(){
-	
+//manual entry of location NOT WORKING
+	// function outPut(){
+		// var userInput = document.getElementById("desLocation").value;
+		// console.log(userInput);
+	// }
+	// outPut;
+//	$("#location").html()
 	
 	if (navigator.geolocation) {
 		
@@ -8,15 +14,16 @@ $(document).ready(function(){
 			lon = position.coords.longitude;
 			
 			var api = 'http://api.openweathermap.org/data/2.5/weather?lat='+lat+'&lon='+lon+'&appid=0a37ba98b900f7cfeb11629c1ee0d7d5';
-			
+			console.log(api);
 			$.getJSON(api, function(data){
 
 //icon
 				var icon = data.weather[0].id
-				$('#icon').html("<i class='owf owf-" + icon + " owf-5x'></i>");	
+				$('#icon').html("<i class='wi wi-owm-" + icon + "'></i>");
+				
 //City Name
 				$('#location').html("<span class='type'>City: </span>"+JSON.stringify(data.name));			
-//Weather Description and backgrounds	NEEDS WORK		
+//Weather Description and backgrounds		
 				weatherDescription = data.weather[0].main;
 				$('#weatherDescription').html("<span class='type'>Condition: </span>"+weatherDescription);
 				
@@ -24,26 +31,48 @@ $(document).ready(function(){
 					$('body').css('background-image', 'url(https://static.pexels.com/photos/132983/pexels-photo-132983.jpeg)');
 				}	
 				
-				backgroundImg = {
-					thunderStormD : "200-299",
-					drizzleD : "300-399",
-					rainD : "500-599",
-					snowD : "600-699",
-					atmosphereD : "700-799",
-					clearD : "800",
-					cloudsD : "801-804",
-					extremeD : "900-999",
+				backgroundImgContainer = {
+					thunderStormD : "http://i.imgur.com/d8i9wi6.jpg",
+					drizzleD : "http://i.imgur.com/q5J4JPK.jpg",
+					rainD : "http://i.imgur.com/q5J4JPK.jpg",
+					snowD : "http://i.imgur.com/IfbOdJ2.jpg",
+					atmosphereD : "http://i.imgur.com/XtTIvYL.jpg",
+					clearD : "http://i.imgur.com/udSev3z.jpg",
+					cloudsD : "http://i.imgur.com/JnJXVe4.jpg",
+					extremeD : "http://i.imgur.com/SdwZZkk.jpg",
 				}
-				//start here
-				switch(){
-					case 0:
-						data.weather[0].main < 299
+				backgroundImg = ''
+				
+				
+				switch(icon){
+					case icon <299:
+						backgroundImg = backgroundImgContainer["thunderStormD"];
 						break;
+					case icon >= 300 && icon <=399 :
+						backgroundImg = backgroundImgContainer["drizzleD"];
+						break;
+					case icon >= 500 && icon <=599:
+						backgroundImg = backgroundImgContainer["rainD"];
+						break;
+					case icon >= 600 && icon <=699:
+						backgroundImg = backgroundImgContainer["snowD"];
+						break;
+					case icon >= 700 && icon <=799:
+						backgroundImg = backgroundImgContainer["atmosphereD"];
+						break;
+					case icon = 800:
+						backgroundImg = backgroundImgContainer["clearD"];
+						break;
+					case icon >= 801 && icon <=804:
+						backgroundImg = backgroundImgContainer["cloudsD"];
+						break;
+					case icon >= 805 && icon <=999:
+						backgroundImg = backgroundImgContainer["extremeD"];
+						break;
+					default: console.log("oops");
 				}
-					
+				$('body').css('background-image', 'url('+backgroundImg);
 				
-				
-
 //Sunrise and set time NEEDS WORK
 				new Date(data.sys.sunrise * 1000);
 				$('#time').html(Date);
@@ -53,20 +82,19 @@ $(document).ready(function(){
 				cTemp = ((fTemp - 32) * (5/9));
 					$('#temp-container').html(Math.round(fTemp) + '&deg;F');				
 				
-			});
-			
-			
+			});						
 		});
 		
-//Temperature Buttons
+
+   
+	} 
+	//Temperature Buttons
 		$('#cButton').click(function(){
 			$('#temp-container').html(Math.round(cTemp) + '&deg;C');
 		});		
 		$('#fButton').click(function(){
 			$('#temp-container').html(Math.round(fTemp) + '&deg;F');
 		});
-   
-	};
 });
   
   
