39b2d8a5e17ecf041de0ab94c9f15f02878303b8
Daniel Alexander <davisd49@msu.edu>
2016-12-19 17:11:44 -0500

    added inputs and styled

--- scripts/script.js
+++ scripts/script.js
@@ -1,13 +1,88 @@
 $(document).ready(function(){
-//manual entry of location NOT WORKING
-	// function outPut(){
-		// var userInput = document.getElementById("desLocation").value;
-		// console.log(userInput);
-	// }
-	// outPut;
-//	$("#location").html()
+
+		$("#searchForm").submit(function(e){
+			searchInput = $("#searchInput").val();
+			address = "api.openweathermap.org/data/2.5/weather?q="+searchInput+"&appid=0a37ba98b900f7cfeb11629c1ee0d7d5";
+			
+			
+			console.log(address);
 	
-	if (navigator.geolocation) {
+			 $.getJSON(address, function(data){
+				var icon = data.weather[0].id
+					$('#icon').html("<i class='wi wi-owm-" + icon + "'></i>");
+					
+	//City Name
+					$('#location').html("<span class='type'>City: </span>"+JSON.stringify(data.name));			
+	//Weather Description and backgrounds		
+					weatherDescription = data.weather[0].main;
+					$('#weatherDescription').html("<span class='type'>Condition: </span>"+weatherDescription);
+					
+					
+					
+					backgroundImgContainer = {
+						thunderStormD : "https://s26.postimg.org/5jz02h8i1/d8i9wi6.jpg",
+						drizzleD : "https://s26.postimg.org/mobprer0p/Jn_JXVe4.jpg",
+						rainD : "https://s26.postimg.org/mobprer0p/Jn_JXVe4.jpg",
+						snowD : "https://s26.postimg.org/4k8p6rtc9/Ifb_Od_J2.jpg",
+						atmosphereD : "https://s26.postimg.org/vede5zxvt/Xt_TIv_YL.jpg",
+						clearD : "https://s26.postimg.org/q0elyg85l/udSev3z.jpg",
+						cloudsD : "https://s26.postimg.org/mobprer0p/Jn_JXVe4.jpg",
+						extremeD : "https://s26.postimg.org/re68tr7ex/Sdw_ZZkk.jpg",
+					}
+					backgroundImg = ''
+					
+					
+					if(icon < 299){
+						backgroundImg = backgroundImgContainer["thunderStormD"];
+					} if (icon >= 300 && icon <=399){
+						backgroundImg = backgroundImgContainer["drizzleD"];
+					}if (icon >= 500 && icon <=599){
+						backgroundImg = backgroundImgContainer["rainD"];
+					}if (icon >= 600 && icon <=699){
+						backgroundImg = backgroundImgContainer["snowD"];
+					}if (icon >= 700 && icon <=799){
+						backgroundImg = backgroundImgContainer["atmosphereD"];
+					}
+	//icon id weirdness, if cloudsD range is 800-804, shows clearD when ID is 804, should be cloudsD. 				
+					if (icon = 800){ 
+						backgroundImg = backgroundImgContainer["clearD"];
+					}if (icon = 804){
+						backgroundImg = backgroundImgContainer["cloudsD"];
+					}if (icon >= 805 && icon <=999){
+						backgroundImg = backgroundImgContainer["extremeD"];
+					}
+						
+					$('body').css('background-image', 'url('+backgroundImg);
+					
+	//Sunrise and set time NEEDS WORK
+					new Date(data.sys.sunrise * 1000);
+					$('#time').html(Date);
+	//Temperature				
+					kTemp = data.main.temp;
+					fTemp = Math.round(kTemp * (9/5) - 459.67);
+					cTemp = ((fTemp - 32) * (5/9));
+						$('#temp-container').html(Math.round(fTemp) + '&deg;F');				
+					
+				
+			 });
+					
+					
+		// 'No results' doesn't work
+		// if ($('#output') == ""){
+			 // $('#output').text('<li>Your search turned up nothing</li>')
+		// }
+			//$("html, body").animate({ scrollTop: "550px" },1000);
+			e.preventDefault();		
+		});
+		
+		$("#searchInput").keypress(function(e){
+			if(e.which == 13){
+				$("#search").click();
+			};
+		});
+
+
+	 if (navigator.geolocation) {
 		
 		navigator.geolocation.getCurrentPosition(function(position){
 			lat = position.coords.latitude;
@@ -15,68 +90,73 @@ $(document).ready(function(){
 			
 			var api = 'http://api.openweathermap.org/data/2.5/weather?lat='+lat+'&lon='+lon+'&appid=0a37ba98b900f7cfeb11629c1ee0d7d5';
 			console.log(api);
-			$.getJSON(api, function(data){
+			
+			function getStuff(){
+				$.getJSON(api, function(data){
 
-//icon
-				var icon = data.weather[0].id
-				$('#icon').html("<i class='wi wi-owm-" + icon + "'></i>");
-				
-//City Name
-				$('#location').html("<span class='type'>City: </span>"+JSON.stringify(data.name));			
-//Weather Description and backgrounds		
-				weatherDescription = data.weather[0].main;
-				$('#weatherDescription').html("<span class='type'>Condition: </span>"+weatherDescription);
-				
-				
-				
-				backgroundImgContainer = {
-					thunderStormD : "http://i.imgur.com/d8i9wi6.jpg",
-					drizzleD : "http://i.imgur.com/q5J4JPK.jpg",
-					rainD : "http://i.imgur.com/q5J4JPK.jpg",
-					snowD : "http://i.imgur.com/IfbOdJ2.jpg",
-					atmosphereD : "http://i.imgur.com/XtTIvYL.jpg",
-					clearD : "http://i.imgur.com/udSev3z.jpg",
-					cloudsD : "http://i.imgur.com/JnJXVe4.jpg",
-					extremeD : "http://i.imgur.com/SdwZZkk.jpg",
-				}
-				backgroundImg = ''
-				
-				
-				if(icon < 299){
-					backgroundImg = backgroundImgContainer["thunderStormD"];
-				} else if (icon >= 300 && icon <=399){
-					backgroundImg = backgroundImgContainer["drizzleD"];
-				}else if (icon >= 500 && icon <=599){
-					backgroundImg = backgroundImgContainer["rainD"];
-				}else if (icon >= 600 && icon <=699){
-					backgroundImg = backgroundImgContainer["snowD"];
-				}else if (icon >= 700 && icon <=799){
-					backgroundImg = backgroundImgContainer["atmosphereD"];
-				}else if (icon = 800){
-					backgroundImg = backgroundImgContainer["clearD"];
-				}else if (icon >= 801 && icon <=804){
-					backgroundImg = backgroundImgContainer["cloudsD"];
-				}else if (icon >= 805 && icon <=999){
-					backgroundImg = backgroundImgContainer["extremeD"];
-				}
+	//icon
+					var icon = data.weather[0].id
+					$('#icon').html("<i class='wi wi-owm-" + icon + "'></i>");
 					
-				$('body').css('background-image', 'url('+backgroundImg);
-				
-//Sunrise and set time NEEDS WORK
-				new Date(data.sys.sunrise * 1000);
-				$('#time').html(Date);
-//Temperature				
-				kTemp = data.main.temp;
-				fTemp = Math.round(kTemp * (9/5) - 459.67);
-				cTemp = ((fTemp - 32) * (5/9));
-					$('#temp-container').html(Math.round(fTemp) + '&deg;F');				
-				
-			});						
-		});
-		
-
-   
-	} 
+	//City Name
+					$('#location').html("<span class='type'>City: </span>"+JSON.stringify(data.name));			
+	//Weather Description and backgrounds		
+					weatherDescription = data.weather[0].main;
+					$('#weatherDescription').html("<span class='type'>Condition: </span>"+weatherDescription);
+					
+					
+					
+					backgroundImgContainer = {
+						thunderStormD : "https://s26.postimg.org/5jz02h8i1/d8i9wi6.jpg",
+						drizzleD : "https://s26.postimg.org/mobprer0p/Jn_JXVe4.jpg",
+						rainD : "https://s26.postimg.org/mobprer0p/Jn_JXVe4.jpg",
+						snowD : "https://s26.postimg.org/4k8p6rtc9/Ifb_Od_J2.jpg",
+						atmosphereD : "https://s26.postimg.org/vede5zxvt/Xt_TIv_YL.jpg",
+						clearD : "https://s26.postimg.org/q0elyg85l/udSev3z.jpg",
+						cloudsD : "https://s26.postimg.org/mobprer0p/Jn_JXVe4.jpg",
+						extremeD : "https://s26.postimg.org/re68tr7ex/Sdw_ZZkk.jpg",
+					}
+					backgroundImg = ''
+					
+					
+					if(icon < 299){
+						backgroundImg = backgroundImgContainer["thunderStormD"];
+					} if (icon >= 300 && icon <=399){
+						backgroundImg = backgroundImgContainer["drizzleD"];
+					}if (icon >= 500 && icon <=599){
+						backgroundImg = backgroundImgContainer["rainD"];
+					}if (icon >= 600 && icon <=699){
+						backgroundImg = backgroundImgContainer["snowD"];
+					}if (icon >= 700 && icon <=799){
+						backgroundImg = backgroundImgContainer["atmosphereD"];
+					}
+	//icon id weirdness, if cloudsD range is 800-804, shows clearD when ID is 804, should be cloudsD. 				
+					if (icon = 800){ 
+						backgroundImg = backgroundImgContainer["clearD"];
+					}if (icon = 804){
+						backgroundImg = backgroundImgContainer["cloudsD"];
+					}if (icon >= 805 && icon <=999){
+						backgroundImg = backgroundImgContainer["extremeD"];
+					}
+						
+					$('body').css('background-image', 'url('+backgroundImg);
+					
+	//Sunrise and set time NEEDS WORK
+					new Date(data.sys.sunrise * 1000);
+					$('#time').html(Date);
+	//Temperature				
+					kTemp = data.main.temp;
+					fTemp = Math.round(kTemp * (9/5) - 459.67);
+					cTemp = ((fTemp - 32) * (5/9));
+						$('#temp-container').html(Math.round(fTemp) + '&deg;F');				
+					
+				});						
+			};
+	//tried to use this function for search
+			getStuff();
+		}); 
+	  
+	}	
 	//Temperature Buttons
 		$('#cButton').click(function(){
 			$('#temp-container').html(Math.round(cTemp) + '&deg;C');
